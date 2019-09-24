fe9992f7a71617b0dc4193a7b7674e47944760ec
unknown <lildykai@gmail.com>
2019-09-24 09:11:17 -0700

    view id params

--- index.js
+++ index.js
@@ -4,17 +4,45 @@ const pug = require("pug");
 const port = 3000;
 var mongoose = require('mongoose');
 var Post = require('./models/post.models');
+var bodyParser = require('body-parser');
 
 
-mongoose.connect('mongodb://localhost:27017/bachkhoa', {useNewUrlParser: true});
+mongoose.connect('mongodb://localhost:27017/bachkhoa', {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
 
 app.set('view engine', 'pug');
-app.set('views', './views')
+app.set('views', './views');
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 app.get('/post', async function(req, res){
     var post = await Post.find();
-    console.log(post)
     res.render('post/index',{
+        post:post
+    })
+})
+
+app.get('/create', function(req, res){
+    res.render('create/index')
+})
+
+app.post('/create', async function(req, res){
+    var posts = new Post(req.body);
+    console.log(posts)
+    var post = await Post.find();
+    posts.save(function(error){
+        if(error)
+            return res.json({ posts : posts })
+    })
+    res.redirect('/post')
+
+})
+
+app.get('/post:id', async function(req, res){
+    var id = req.params.id;
+    console.log(id)
+    var post = await Post.find();
+    res.render('post/view', {
         post:post
     })
 })
--- models/post.models.js
+++ models/post.models.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 
 var postSchema = new mongoose.Schema({
 	name : String,
+	conten: String
 });
 
 var Post = mongoose.model('Post', postSchema, 'post');
